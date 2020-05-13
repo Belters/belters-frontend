91e7beed69302236239c647460e44d87f354245e
kshitiz <kshit96@gmail.com>
2020-05-13 16:29:27 +0700

    Create Backgroung imageRwo component with images in square grid for login page

---
+++ lib/MyApp.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'imageRow.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final double height = MediaQuery.of(context). size. height;
+    final double width = (MediaQuery.of(context).size.width)/4;
+    final numberOfRows= height/width;
+
+    _createChildren(){
+      List<Widget> returnRows = [];
+      for(int i=0;i<numberOfRows+1;i++){
+        returnRows.add(imageRow());
+      }
+      return returnRows;
+    }
+
+    return Scaffold(
+//        appBar: AppBar(
+//          title: Text('Belters App'),
+//        ),
+        body: Center(
+            child: Column(
+                children: _createChildren()
+            )
+        )
+    );
+  }
+}
---
+++ lib/imageRow.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+class imageRow extends StatelessWidget{
+  Widget build(BuildContext context){
+
+    final double height = MediaQuery.of(context).size.height;
+    final double width = (MediaQuery.of(context).size.width)/4;
+    final double numberOfRows= height/width;
+    print(height);
+    print(width);
+    print(numberOfRows);
+
+    return  Row(
+      children: [
+        Image.asset('assets/images/afridiShout.png',
+          fit: BoxFit.cover,
+          width: width,
+          height: height/numberOfRows,
+        ),
+        Image.asset('assets/images/dhoniStare.png',
+          fit: BoxFit.cover,
+          width: width,
+          height: height/numberOfRows,
+        ),
+        Image.asset('assets/images/mithaliRaj.png',
+          fit: BoxFit.cover,
+          width: width,
+          height: height/numberOfRows,
+        ),
+        Image.asset('assets/images/stokesKohliWicket.png',
+          fit: BoxFit.cover,
+          width: width,
+          height: height/numberOfRows,
+        ),
+
+      ],
+    );
+  }
+}
